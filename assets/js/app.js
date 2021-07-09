b33285fbd6653e949d8fc202723e77c7ffa864f5
Jordan Roessle <screamaimfire@me.com>
2021-07-08 22:24:41 -0700

    added axis labels and now correctly displays all text on circles

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 function resizeChart() {
 
     // select svg area
-    var svgArea = d3.select("body").select("svg");
+    var svgArea = d3.select("#scatter").select("svg");
 
     // clear svg 
     if (!svgArea.empty()) {
@@ -43,7 +43,6 @@ function resizeChart() {
             data.smokes = +data.smokes;
         });
 
-
         // create scales
         var xLinearScale = d3.scaleLinear()
             .domain([d3.min(stateData, d => d.age), d3.max(stateData, d => d.age)])
@@ -54,18 +53,19 @@ function resizeChart() {
             .range([height, 0]);
     
         // create axes
-        var xAxis = d3.axisBottom(xLinearScale);
-        var yAxis = d3.axisLeft(yLinearScale);
+        var bottomAxis = d3.axisBottom(xLinearScale);
+        var leftAxis = d3.axisLeft(yLinearScale);
 
         // append axes
         chartGroup.append("g")
             .attr("transform", `translate(0, ${height})`)
-            .call(xAxis);
+            .call(bottomAxis);
 
         chartGroup.append("g")
-            .call(yAxis);
+            .call(leftAxis);
 
         var radiusSize = 10;
+
         // append circles
         var circlesGroup = chartGroup.selectAll("circle")
             .data(stateData)
@@ -74,21 +74,64 @@ function resizeChart() {
             .attr("cx", d => xLinearScale(d.age))
             .attr("cy", d => yLinearScale(d.smokes))
             .attr("r", radiusSize.toString())
-            .attr("class", "stateCircle")
-            .text(d => d.abbr);
+            .attr("class", "stateCircle");
+            
         
         // add text to each circle
-        chartGroup.selectAll("text")
+        var textGroup = chartGroup.selectAll("aText")
             .data(stateData)
             .enter()
             .append("text")
             .attr("x", d => xLinearScale(d.age))
             .attr("y", d => yLinearScale(d.smokes))
-            .attr("dy", "2px")
+            .attr("dy", "3px")
             .attr("font-size", `${radiusSize*.9}px`)
-            //.attr("class", "stateText")
-            .text(d => d.state);
+            .attr("class", "stateText")
+            .text(d => d.abbr);
         
+        // initialize tooltip
+        var toolTip = d3.tip()
+            .attr("class", "d3-tip")
+            .offset([80, -60])
+            .html(function(d) {
+                return (`<strong>${d.state}</strong><br>
+                <strong>Age: ${d.age}</strong><br>
+                <strong>Smokes: ${d.smokes}</strong><br>`)
+            });
+        
+        // call tootlip 
+        chartGroup.call(toolTip);
+
+        // add mouseover and mouse out for both circles and text
+        circlesGroup.on("mouseover", function(d) {
+            toolTip.show(d, this);
+          })
+            .on("mouseout", function(d) {
+              toolTip.hide(d);
+          });
+        textGroup.on("mouseover", function(d) {
+            toolTip.show(d, this);
+          })
+            .on("mouseout", function(d) {
+              toolTip.hide(d);
+          });
+
+        // add axis labels
+        chartGroup.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left)
+            .attr("x", 0 - (height / 2))
+            .attr("dy", "1em")
+            .attr("class", "active aText")
+            .text("Smokes");
+
+        chartGroup.append("text")
+            .attr("transform", `translate(${width / 2}, ${height + margin.top - 10})`)
+            .attr("class", "active aText")
+            .text("Age");
+        
+
+
 
     }).catch(function(error) {
         console.log(error);
